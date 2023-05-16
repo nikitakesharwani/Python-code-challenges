b1b3fdc470bca7c29d24c1ba378cbdc6931ec09b
nikitakesharwani <nikitakesharwani1708@gmail.com>
2023-05-16 20:42:13 +0530

    implemented set operations

---
+++ set_operations.py
@@ -0,0 +1,15 @@
+set1 = {1,2,3,4,5,6}
+set2 = {4,3,7,8,1,9}
+
+union_set = set1.union(set2)
+print(union_set)
+
+intersection_set = set1.intersection(set2)
+print(intersection_set)
+
+difference_set1set2 = set1.difference(set2)
+print(difference_set1set2)
+
+difference_set2set1 = set2.difference(set1)
+print(difference_set2set1)
+
